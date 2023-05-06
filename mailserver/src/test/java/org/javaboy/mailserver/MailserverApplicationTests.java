ee82c276ee4fda2b79652718850f37694ef90538
oycm <1164864987@qq.com>
2023-05-06 16:55:57 +0800

    类添加修饰符public，否则@Test不能使用，同时测试方法需要用public修饰

--- mailserver/src/test/java/org/javaboy/mailserver/MailserverApplicationTests.java
+++ mailserver/src/test/java/org/javaboy/mailserver/MailserverApplicationTests.java
@@ -1,13 +1,41 @@
 package org.javaboy.mailserver;
 
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class MailserverApplicationTests {
+public class MailserverApplicationTests {
+
+    @Autowired
+    JavaMailSender javaMailSender;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+    }
+
+
+    @Test
+    public void sendSimpleMail() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("这是一封测试邮件");
+        message.setFrom("1164864987@qq.com");
+        message.setTo("mingorg1024@gmail.com");
+        message.setCc("mingorg@163.com");
+        //message.setBcc("1164864987@qq.com");
+        message.setSentDate(new Date());
+        message.setText("这是测试邮件的正文");
+        javaMailSender.send(message);
+        System.out.println(javaMailSender);
+
     }
 
 }
