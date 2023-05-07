951008dd90acf74038015f990329ae4036e6ac44
oycm <1164864987@qq.com>
2023-05-07 15:02:58 +0800

    配置交换机，队列，绑定交换机队列关系。 RedisTemplate设置public-confirms，public-returns的回调函数

--- vhrserver/vhr-service/src/main/java/org/javaboy/vhr/config/RabbitConfig.java
+++ vhrserver/vhr-service/src/main/java/org/javaboy/vhr/config/RabbitConfig.java
@@ -25,6 +25,9 @@ public class RabbitConfig {
     @Bean
     RabbitTemplate rabbitTemplate() {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(cachingConnectionFactory);
+        // 生产者通过将消息发布到一个交换机，如果交换机成功接收了这个消息，RabbitMQ 会向生产者发送一个确认消息，触发这个回调函数，其中ack为true
+        // 如果生产者在规定的时间内没有收到来自RabbitMQ的确认消息，就会触发回调函数，ack为false
+        // 消息发送成功触发的回调函数
         rabbitTemplate.setConfirmCallback((data, ack, cause) -> {
             String msgId = data.getId();
             if (ack) {
@@ -34,6 +37,8 @@ public class RabbitConfig {
                 logger.info(msgId + ":消息发送失败");
             }
         });
+        // 这里设置生产者发送到RabbitMQ无法路由到队列时设置的回调函数
+        // 需要将publisher-returns 设置为true还有消失生产时设置了mandatory
         rabbitTemplate.setReturnCallback((msg, repCode, repText, exchange, routingkey) -> {
             logger.info("消息发送失败");
         });
