17789aba69ee23faf0a3a786a87b08de853797f0
oycm <1164864987@qq.com>
2023-05-07 15:15:28 +0800

    socket消息代理配置

--- vhrserver/vhr-web/src/main/java/org/javaboy/vhr/config/WebSocketConfig.java
+++ vhrserver/vhr-web/src/main/java/org/javaboy/vhr/config/WebSocketConfig.java
@@ -11,11 +11,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // /ws/ep是注册的STOMP节点
+        // setAllowedOrigins("*")方法允许所有来源的主机都可以连接,withSockJS()方法表示启用SockJS支持
         registry.addEndpoint("/ws/ep").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //使用enableSimpleBroker方法启用简单的基于内存的消息代理，可以使用/queue前缀过滤目的地为queue的消息。
+        // 在客户端发送消息时，必须先订阅对应的目的地，才能接收到该目的地的消息
         registry.enableSimpleBroker("/queue");
     }
 }
